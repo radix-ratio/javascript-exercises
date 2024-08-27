76150c179ca6e4405c71883ea8d168f45d9f41ca
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-08-27 23:38:47 +0200

    add reversedString function logic

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedStringArr = [];
 
+    for (let i = string.length-1; i >= 0; i--) {
+        reversedStringArr.push(string[i]);
+    }
+
+    return reversedStringArr.join("");
 };
 
 // Do not edit below this line
