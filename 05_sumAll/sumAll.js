3ac86905ef9296042608c712c0287f2c0d53a773
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-08-28 00:26:05 +0200

    add sumAll function logic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    if (startNumber < 0 || endNumber < 0) {
+        return "ERROR";
+    } else if (typeof startNumber === "string" || typeof endNumber === "string") {
+        return "ERROR";
+    } else if (Number(startNumber) === startNumber  && startNumber % 1 !== 0|| Number(endNumber) === endNumber && endNumber % 1 !== 0) {
+        return "ERROR";
+    } else if (typeof startNumber !== "number" || typeof endNumber !== "number") {
+        return "ERROR";
+    } else if (startNumber > endNumber) {
+        [startNumber, endNumber] = [endNumber, startNumber];
+    } 
+    let sum = 0;
+    for (let i = startNumber; i <= endNumber; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
