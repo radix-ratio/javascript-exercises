d3c915a5cfc1f8ae71921e8a8a6244c8bd9e73d8
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-09-06 12:00:47 +0200

    add fibonnaci sequence function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num === 0) return num;
+    if ( num === 1 || num === 2) return 1;
+    //To learn more about Fibonacci sequences, go to: https://en.wikipedia.org/wiki/Fibonacci_sequence
+    const fibNumbers = [0, 1, 1];
+    for (let i=3; i<=num; i++) {
+        fibNumbers[i] = fibNumbers[i-1] + fibNumbers[i-2];
+    }
+    return fibNumbers[num];
 };
 
 // Do not edit below this line
