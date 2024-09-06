d1d92883c934add7d4420a454bf0251abe476a1a
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-09-06 12:19:13 +0200

    add function to get titles from array of objects

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // return  books.reduce((titles, book) =>{
+    //     if (!titles.includes(book.title)) {
+    //         titles.push(book.title);
+    //     }
+    //     return titles;
+    // }, []);
+    return books.map((book) => book.title);
 };
 
 // Do not edit below this line
