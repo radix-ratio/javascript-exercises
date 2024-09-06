dc8f89211e8547672ff005ca79c2f070c633a8ec
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-09-06 11:48:44 +0200

    add palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[:;!?.,]/g, "").split(" ").join("");
+    return str.split("").reverse().join('') === str;
 };
 
 // Do not edit below this line
